3e7a3405f75f6d9fd323e97ece46317588fd4c9d
Johnabhi9514 <johnabhi9514@gmail.com>
2026-01-30 15:34:51 +0530

    requirement added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RAG_Based_Medical_Chatbot
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RAG_Based_Medical_Chatbot",
+    version="0.1.0",
+    author="Abhishek Singh",
+    author_email="johnabhi9514@gmail.com",
+    description="A RAG-based medical chatbot",
+    packages=find_packages(),
+    install_requires=[]
+)
